9de237a58978b71121a98c66c8ce26471bc9f4ec
jakubeva <850778435@qq.com>
2023-06-30 15:52:59 +0800

    增加DepartmentService注释

--- apartment-management/apartment-management-service/src/main/java/edu/npu/service/DepartmentService.java
+++ apartment-management/apartment-management-service/src/main/java/edu/npu/service/DepartmentService.java
@@ -14,21 +14,51 @@ import org.springframework.transaction.annotation.Transactional;
 */
 public interface DepartmentService extends IService<Department> {
 
+    /**
+     *新增外部单位信息
+     * @param departmentDto 外部单位信息
+     * @return R 成功或失败信息
+     */
     @Transactional(rollbackFor = Exception.class)
     R addDepartment(DepartmentDto departmentDto);
 
+    /**
+     * 删除外部单位
+     * @param id 外部单位id
+     * @return R 成功或失败信息
+     */
     @Transactional(rollbackFor = Exception.class)
     R deleteDepartment(Long id);
 
+    /**
+     * 修改外部单位信息
+     * @param id 修改外部单位信息
+     * @param departmentDto 外部单位信息
+     * @return R 修改结果成功或失败信息
+     */
     @Transactional(rollbackFor = Exception.class)
     R updateDepartment(Long id, DepartmentDto departmentDto);
 
+    /**
+     * 查询外部单位信息列表
+     * @param departmentPageQueryDto 查询条件
+     * @return R 外部单位列表
+     */
     @Transactional(rollbackFor = Exception.class)
     R getDepartmentList(DepartmentPageQueryDto departmentPageQueryDto);
 
+    /**
+     * 查询外部单位信息简表
+     * @return 简表
+     */
     @Transactional(rollbackFor = Exception.class)
     R getDepartmentSimpleList();
 
+    /**
+     * 查看外部单位详细信息
+     * @param id 外部单位ID
+     * @return R
+     */
     @Transactional(rollbackFor = Exception.class)
     R getDepartmentDetail(Long id);
 
