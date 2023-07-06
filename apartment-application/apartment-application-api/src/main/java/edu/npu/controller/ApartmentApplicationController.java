1765b8370ee21fb1897e600633a2e5ab77633a43
WangMinan <wangminan0811@hotmail.com>
2023-07-06 16:00:06 +0800

    更新搜索逻辑

--- apartment-application/apartment-application-api/src/main/java/edu/npu/controller/ApartmentApplicationController.java
+++ apartment-application/apartment-application-api/src/main/java/edu/npu/controller/ApartmentApplicationController.java
@@ -9,10 +9,7 @@ import jakarta.annotation.Resource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : [wangminan]
@@ -38,7 +35,7 @@ public class ApartmentApplicationController {
     @PostMapping
     @PreAuthorize("hasAuthority('CENTER_DORM_MANAGER')")
     public R statusChangeConfirm(
-            @Validated BasicReviewDto reviewDto
+            @Validated @RequestBody BasicReviewDto reviewDto
     ) {
         return applicationService.statusChangeConfirm(reviewDto);
     }
